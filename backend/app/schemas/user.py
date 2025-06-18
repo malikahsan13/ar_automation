fd3e305560feef730c655a94334d6b6bb454bbf8
Malik Ahsan <malikahsan13@gmail.com>
2025-06-18 16:31:06 +0500

    created user schemas

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,1 +1,18 @@
 from pydantic import BaseModel, EmailStr
+
+class UserCreatE(BaseModel):
+    email: EmailStr
+    password: str
+    
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+    
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    
+    class config:
+        orm_mode = True
