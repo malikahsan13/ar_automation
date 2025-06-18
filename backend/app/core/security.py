f6e0567e0dbfc3316b6f244b4da3fa7c489d92d1
Malik Ahsan <malikahsan13@gmail.com>
2025-06-18 16:57:26 +0500

    added code for creating token hashing password and decoding token

---
+++ backend/app/core/security.py
@@ -0,0 +1,31 @@
+from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+
+
+SECRET_KEY = os.getenv("SECRET_KEY", "12345")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashedPassword: str):
+    return pwd_context.verify(password, hashedPassword)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except:
+        return None
