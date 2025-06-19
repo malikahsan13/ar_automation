236491062e4dcdb3ce15a63b1110a4789f458b57
Malik Ahsan <malikahsan13@gmail.com>
2025-06-19 20:50:47 +0500

    added logic in main file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import router from './router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .mount('#app')
