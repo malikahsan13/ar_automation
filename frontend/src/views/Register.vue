d11beafc483b096e43a161ad21bbee712d59c1cb
Malik Ahsan <malikahsan13@gmail.com>
2025-06-19 20:49:47 +0500

    register page logic added

---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Register</h2>
+    <form @submit.prevent="register">
+      <input v-model="email" placeholder="Email" />
+      <input v-model="password" placeholder="Password" type="password" />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+
+const email = ref('')
+const password = ref('')
+const router = useRouter()
+
+const register = async () => {
+  await axios.post('http://localhost:8000/auth/register', {
+    email: email.value,
+    password: password.value
+  })
+  router.push('/login')
+}
+</script>
