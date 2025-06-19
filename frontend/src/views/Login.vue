e4a0322521eb8b31dfc7650630101779cd0c8cdc
Malik Ahsan <malikahsan13@gmail.com>
2025-06-19 20:50:11 +0500

    added login page code

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input v-model="email" placeholder="Email" />
+      <input v-model="password" placeholder="Password" type="password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '../store/auth'
+
+const router = useRouter()
+const email = ref('')
+const password = ref('')
+const auth = useAuthStore()
+
+const login = async () => {
+  await auth.login(email.value, password.value)
+  router.push('/dashboard') // create this later
+}
+</script>
